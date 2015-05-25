386876e98a40da94a6493088248c5762b1b2600b
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-05-24 21:49:00 -0500

    main lib

---
+++ src/main.js
@@ -0,0 +1,13 @@
+module.exports = objarray;
+
+function objarray(arr, obj) {
+	if (!arr.length) return [];
+	if (arr.length === 1) return arr.slice(0);
+
+	var items = [arr[0]];
+	for (var i = 1, len = arr.length; i < len; ++i) {
+		items.push(obj, arr[i]);
+	}
+
+	return items;
+}
